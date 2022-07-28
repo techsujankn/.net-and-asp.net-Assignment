447de98a577fcb35e55bed54938a4d9cf7457b37
techsujankn <43318418+techsujankn@users.noreply.github.com>
2022-07-28 09:38:48 +0530

    Add files via upload

---
+++ repos/MainProject/MainProject/Employee.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+namespace MainProject
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public string Gender { get; set; }
+        public double Salary { get; set; }
+
+        List<Employee> employeelist = new List<Employee>();
+
+        public string Login(string userid, string password)
+        {
+            if(string.IsNullOrEmpty(userid) || string.IsNullOrEmpty(password))
+            {
+                return "User Id or Password cannot be Empty";
+            }
+            else if (userid=="Admin" && password=="Admin123")
+            {
+                return "Welcome Admin";
+            }
+            else
+                return "Invalid User Id or Password";
+        }
+
+        public List<Employee> AllEmployess()
+        {
+            employeelist.Add(new Employee { ID = 100, Fname = "Parihar", Lname = "Kushwant", Gender = "Male", Salary = 35000 });
+            employeelist.Add(new Employee { ID = 101, Fname = "Meghana", Lname = "Kurimi", Gender = "Female", Salary = 40000 });
+            employeelist.Add(new Employee { ID = 102, Fname = "Deepak", Lname = "S", Gender = "Male", Salary = 45000 });
+            employeelist.Add(new Employee { ID = 103, Fname = "Aakash", Lname = "Aanchal", Gender = "Male", Salary = 37000 });
+
+            return employeelist;
+        }
+
+        public string GetName(string fname, string lname)
+        {
+            return string.Concat(fname + lname);
+        }
+
+        public List<Employee> GetEmployeeById(int id) 
+        {
+            List<Employee> e = (from emp in employeelist
+                                where emp.ID == id
+                                select emp).ToList();
+
+            if (e != null)
+            {
+                return e;
+            }
+            else
+                e = null;
+            return e;
+        }
+
+        public static void Main()
+        {
+
+        }
+    }
+}
---
+++ repos/MainProject/TestingMainProject/EmployeeTester.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using MainProject;
+
+
+namespace TestingMainProject
+{
+    [TestFixture]
+    public class EmployeeTester
+    {
+
+        //Arrange
+        List<Employee> elist;
+
+        [Test]
+        public void CheckAllEmployeedetails()
+        {
+            //Arrange
+
+            Employee emp = new Employee();
+            //Act
+            elist = emp.AllEmployess();
+
+            //Assert
+            foreach (var v in elist)
+            {
+                Assert.IsNotNull(v.ID);
+                Assert.IsNotNull(v.Fname);
+                Assert.IsNotNull(v.Salary);
+            }
+        }
+
+        [Test]
+        public void TestTheEmployeeLoginFunction()
+        {
+            //arrange
+
+            Employee emp = new Employee();
+
+            //act
+
+            string s1 = emp.Login("Deepak", "Admin123");
+            string s2 = emp.Login("", "");
+            string s3 = emp.Login("Admin", "Admin123");
+
+            //assert
+            Assert.AreEqual("Invalid User Id or Password", s1);
+            Assert.AreEqual("Welcome Admin", s3);
+            Assert.AreEqual("User Id or Password cannot be Empty", s2);
+
+        }
+
+        [Test]
+
+        public void GetEmployeebyid_should_return_empty_if_id_doesnotmatch()
+        {
+            //arrange
+            Employee emp = new Employee();
+
+            //act
+            List<Employee> returnedemp = emp.GetEmployeeById(1);
+            //assert
+            Assert.IsNotNull(returnedemp);
+
+            List<Employee> returnedemp1 = emp.GetEmployeeById(101);
+            Assert.IsNotNull(returnedemp1);
+
+        }
+        [Test]
+        public void GetConcatinatedName()
+        {
+            //Arrange
+            Employee emp = new Employee();
+
+            //Act
+            string fname = "Deepak";
+            string lname = " Baravkar";
+            string s1 = emp.GetName("Deepak", "Baravkar");
+            string s2 = emp.GetName(fname, lname);
+
+            //Assert
+            Assert.IsNotNull(s1);
+            Assert.AreEqual(s2, fname + lname);
+        }
+        public static void Main()
+        {
+
+        }
+    }
+}
